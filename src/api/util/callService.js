ee05438003c571e3a433e697b45c69231f005b59
RajendraKumar123 <rajendrakumarvesapogu@gmail.com>
2024-03-21 17:41:22 +0530

    final commit

--- src/api/util/callService.js
+++ src/api/util/callService.js
@@ -4,11 +4,10 @@ dotenv.config();
 module.exports.makeCall = async (toPhoneNumber) => {
 	const accountSid = process.env.TWILIO_SID;
 	const authToken = process.env.TWILIO_AUTH_TOKEN;
-	// console.log(accountSid, authToken);
 	const client = require("twilio")(accountSid, authToken);
 	try {
 		const call = await client.calls.create({
-			url: "http://demo.twilio.com/docs/voice.xml",
+			url: process.env.TWILIO_CALL_URL,
 			to: toPhoneNumber,
 			from: process.env.TWILIO_PHONE_NUMBER,
 		}).then(() => {
